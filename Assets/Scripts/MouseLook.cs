b45cbdab7ba1217347fc67863cc9d3c90ef74bf9
Andrew Lee <alee14498@protonmail.com>
2020-04-19 20:14:07 -0400

    Added watermark and colour to the player

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -2,6 +2,11 @@
 
 public class MouseLook : MonoBehaviour
 {
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +16,11 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        playerBody.Rotate(Vector3.up * mouseX);
+        
         
     }
 }
